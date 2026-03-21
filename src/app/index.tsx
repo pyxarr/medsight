ff385479b4132182f8031dc2ebe305bcca9bc526
Reuben Agbor <reubenagbor10@gmail.com>
2026-03-21 10:44:29 +0100

    chore: configure react native reusables

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,10 @@ export default function Index() {
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
+
+      <Button className="bg-red-600">
+        <Text className="text-white">Button</Text>
+      </Button>
     </View>
   );
 }
