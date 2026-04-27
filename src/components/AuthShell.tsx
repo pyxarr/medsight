23c28e10561556a8773df00537b51f49c64cdbeb
Reuben Agbor <reubenagbor10@gmail.com>
2026-04-27 08:41:35 +0100

    fix

--- src/components/AuthShell.tsx
+++ src/components/AuthShell.tsx
@@ -68,7 +68,7 @@ export function AuthShell({
       <SafeAreaView style={{ flex: 1 }} edges={["bottom", "top"]}>
         <KeyboardAvoidingView
           style={{ flex: 1 }}
-          behavior={Platform.OS === "ios" ? "padding" : undefined}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
           keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
         >
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
